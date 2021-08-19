c8b3de285991b99d83bd8c0fd048f981fd4f03f5
Vladimir Migay <vova.migay123@gmail.com>
2021-08-19 14:39:51 +0300

    Add theme

---
+++ theme/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components/native';
+
+export const theme: DefaultTheme = {
+  colors: {
+    lightGray: '#A1B1C7',
+    blue: '#12c2e9',
+    lightRed: '#f64f59',
+    purple: '#c471ed',
+    white: '#ffffff',
+  },
+};
