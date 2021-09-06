2a3ef7f228c04edd98975541f7fb1c624689fadf
Vladimir Migay <vova.migay123@gmail.com>
2021-09-06 14:50:47 +0300

    Add long press animation

--- app/components/transactions/ListItem.tsx
+++ app/components/transactions/ListItem.tsx
@@ -8,27 +8,62 @@ import { getCategory } from '../../Categories';
 import { Styled } from './styled';
 import { observer } from 'mobx-react-lite';
 import { store } from '../../store/store';
+import { useRef } from 'react';
+import { Animated } from 'react-native';
 
 interface Props {
   title: string;
   money: number;
   date: Date | number;
   isExpense: boolean;
+  isContextModalShow: boolean;
+  onToggleModal: () => void;
 }
 
+const modalTimeForShowMs = 500;
+
 export const ListItem: React.FC<Props> = observer(
-  ({ title, money, date, isExpense }) => {
+  ({ title, money, date, isExpense, isContextModalShow, onToggleModal }) => {
     const formatedMoney = new Intl.NumberFormat(store.user?.locale, {
       style: 'currency',
       currency: store.user?.currency,
     }).format(money);
+    const transformAnim = useRef(new Animated.Value(1)).current;
 
     const formatedDate = moment(date).fromNow();
 
     const category = getCategory(title);
 
+    const onLongPressEnd = () => {
+      Animated.timing(transformAnim, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: false,
+      }).start();
+    };
+
+    const onLongPressStart = () => {
+      Animated.timing(transformAnim, {
+        toValue: 0.9,
+        duration: modalTimeForShowMs,
+        useNativeDriver: false,
+      }).start(({ finished }) => {
+        if (finished) {
+          onToggleModal();
+          onLongPressEnd();
+        }
+      });
+    };
+
     return (
-      <Styled.ItemListContainer>
+      <Animated.View
+        onTouchStart={onLongPressStart}
+        onTouchEnd={onLongPressEnd}
+        style={{
+          paddingBottom: 10,
+          transform: [{ scale: transformAnim }],
+        }}
+      >
         <Styled.ListItem>
           <Styled.TitleContainer>
             <LinearGradient
@@ -58,7 +93,7 @@ export const ListItem: React.FC<Props> = observer(
             <Styled.TransactionDate>{formatedDate}</Styled.TransactionDate>
           </Styled.TransactionInfo>
         </Styled.ListItem>
-      </Styled.ItemListContainer>
+      </Animated.View>
     );
   }
 );
